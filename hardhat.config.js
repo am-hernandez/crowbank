30f2c103c9a3c1e059833684fa1a1c3ed196296b
am-hernandez <angel@mouseandweb.com>
2022-01-06 16:56:59 -0500

    chore: deploy bank and token contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
+const secret = require('./.env/secrets.json');
 
 module.exports = {
   solidity: "0.8.4",
   paths: {
     artifacts: './src/artifacts',
+    tests: './test',
   },
   networks: {
     hardhat: {
       chainId: 1337
-    }
+    },
+    mumbai: {
+      url: secret.mumbaiNode,
+      accounts: [secret.privatekey],
+    },
   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,26 @@
-const hre = require("hardhat");
+const fs = require('fs');
 
 async function main() {
+  const BankContract = await ethers.getContractFactory('CrowBank');
+  const bank = await BankContract.deploy();
+  await bank.deployed();
+  console.log(`The CrowBank Contract was deployed to: ${bank.address}`);
 
+  const TokenContract = await ethers.getContractFactory('Murder');
+  const token = await TokenContract.deploy(bank.address);
+  await token.deployed();
+  console.log(`The Murder Contract was deployed to: ${token.address}`);
+
+  // create the env file with smart contract addresses
+  let addresses = { bankcontract: bank.address, tokencontract: token.address };
+  let addressesJSON = JSON.stringify(addresses);
+  fs.writeFileSync('src/blockchain/contract-address.json', addressesJSON);
 }
 
 main()
-  .then(() => process.exit(0))
+  .then(() => {
+    process.exit(0);
+  })
   .catch((error) => {
     console.error(error);
     process.exit(1);
