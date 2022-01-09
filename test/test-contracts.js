a37c29a6175b78d01315a11a46d78c8b8aa0c790
am-hernandez <angel@mouseandweb.com>
2022-01-09 04:08:42 -0500

    test: add tests for reverting deposit() and mint(), separate withdraw() an deposit() tests

--- test/test-contracts.js
+++ test/test-contracts.js
@@ -1,58 +1,94 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('CrowBank app', () => {
+describe("CrowBank app", () => {
   let bank, token, owner, address_1, address_2;
   let addresses;
+  const one = ethers.utils.parseEther("1.0");
+  const three = ethers.utils.parseEther("3.0");
+  const five = ethers.utils.parseEther("5.0");
+  const nine = ethers.utils.parseEther("9.0");
 
   beforeEach(async () => {
-    const BankContract = await ethers.getContractFactory('CrowBank');
+    const BankContract = await ethers.getContractFactory("CrowBank");
     bank = await BankContract.deploy();
     await bank.deployed();
 
-    const TokenContract = await ethers.getContractFactory('Murder');
+    const TokenContract = await ethers.getContractFactory("Murder");
     token = await TokenContract.deploy(bank.address);
     await token.deployed();
 
     [owner, address_1, address_2, ...addresses] = await ethers.getSigners();
   });
 
-  describe('Deployment', () => {
-    it('Should have totalAssets of 0', async () => {
-      expect(await bank.totalAssets()).to.equal('0');
+  describe("on contracts deployment", () => {
+    it("bank should have total assets of 0", async () => {
+      expect(await bank.totalAssets()).to.equal("0");
     });
-    it('Should have 0 tokens, and 0 deposit in owner account', async () => {
-      expect(await bank.accounts(owner.address)).to.equal('0');
-      expect(await token.balanceOf(owner.address)).to.equal('0');
+    it("owner should have 0 tokens, and 0 deposit", async () => {
+      expect(await bank.accounts(owner.address)).to.equal("0");
+      expect(await token.balanceOf(owner.address)).to.equal("0");
     });
-    it('Should have 0 tokens, and 0 deposit in address_1 account', async () => {
-      expect(await bank.accounts(address_1.address)).to.equal('0');
-      expect(await token.balanceOf(address_1.address)).to.equal('0');
+    it("address_1 should have 0 tokens, and 0 deposit", async () => {
+      expect(await bank.accounts(address_1.address)).to.equal("0");
+      expect(await token.balanceOf(address_1.address)).to.equal("0");
     });
-    it('Should have 0 tokens, and 0 deposit in address_2 account', async () => {
-      expect(await bank.accounts(address_2.address)).to.equal('0');
-      expect(await token.balanceOf(address_2.address)).to.equal('0');
+    it("address_2 should have 0 tokens, and 0 deposit", async () => {
+      expect(await bank.accounts(address_2.address)).to.equal("0");
+      expect(await token.balanceOf(address_2.address)).to.equal("0");
     });
   });
 
-  describe('Deposit and Withdrawal', () => {
-    const oneEth = ethers.utils.parseEther('1.0');
+  describe("bank contract", () => {
+    it("allows addresses to deposit MATIC, and totalAssets() returns sum af all deposits", async () => {
+      await bank.connect(owner).deposit({ value: one });
+      await bank.connect(address_1).deposit({ value: three });
+      await bank.connect(address_2).deposit({ value: five });
+      expect(await bank.accounts(owner.address)).to.equal(one);
+      expect(await bank.accounts(address_1.address)).to.equal(three);
+      expect(await bank.accounts(address_2.address)).to.equal(five);
 
-    it('Should let owner deposit 1 ether, then totalAssets should be 1 ether, and accounts[owner] should be 1 ether', async () => {
-      await bank.connect(address_1).deposit({ value: oneEth });
-      await bank.connect(address_1).withdraw(oneEth, token.address);
-      expect(await bank.totalAssets()).to.equal('0');
-      expect(await token.balanceOf(address_1.address)).to.equal(oneEth);
+      expect(await bank.totalAssets()).to.equal(nine);
     });
-    it('Should let address_1 Deposit and Withdraw 1 ether, then receive 1 FREE', async () => {
-      await bank.connect(address_1).deposit({ value: oneEth });
-      await bank.connect(address_1).withdraw(oneEth, token.address);
-      expect(await bank.totalAssets()).to.equal('0');
-      expect(await token.balanceOf(address_1.address)).to.equal(oneEth);
+
+    it("allows addresses to deposit and withdraw MATIC, then receive 1 MRDR token per withdrawal", async () => {
+      await bank.connect(owner).deposit({ value: one });
+      await bank.connect(address_1).deposit({ value: three });
+      await bank.connect(address_2).deposit({ value: five });
+      await bank.connect(owner).withdraw(one, token.address);
+      await bank.connect(address_1).withdraw(three, token.address);
+      await bank.connect(address_2).withdraw(one, token.address);
+      await bank.connect(address_2).withdraw(one, token.address);
+
+      expect(await bank.totalAssets()).to.equal(ethers.utils.parseEther("3.0"));
+
+      expect(await token.balanceOf(owner.address)).to.equal(one);
+      expect(await token.balanceOf(address_1.address)).to.equal(one);
+      expect(await token.balanceOf(address_2.address)).to.equal(
+        ethers.utils.parseEther("2.0")
+      );
     });
-    it('Should fail when trying to withdraw more money than was deposited', async () => {
+
+    it("withdraw() should fail when trying to withdraw more MATIC than was deposited by address", async () => {
       await expect(
-        bank.connect(address_2).withdraw(oneEth, token.address)
-      ).to.be.revertedWith('Cannot withdraw more than deposited.');
+        bank.connect(address_2).withdraw(one, token.address)
+      ).to.be.revertedWith("Cannot withdraw more than deposited.");
+    });
+
+    it("deposit() should fail when trying to deposit 0 MATIC", async () => {
+      await expect(
+        bank
+          .connect(address_2)
+          .deposit({ value: ethers.utils.parseEther("0.0") })
+      ).to.be.revertedWith("Deposit must be more than 0 MATIC");
+    });
+  });
+
+  describe("token contract", () => {
+    it("mint() should fail when called by any address other than the bank", async () => {
+      await expect(
+        token.connect(bank.signer).mint(owner.address, one)
+      ).to.be.revertedWith("Only the bank can mint new Tokens!");
     });
   });
 });
